d8647324cce5cc0ddc2360baa2189fefd2751e01
KostaSenatov <kostyasenatov@gmail.com>
2022-05-20 01:09:17 +0700

    1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.points__container').click(function(){
+	$('.main-anim').addClass('active');
+
+	$('.points__container').addClass('active');
+	$('.point').addClass('active');
+
+	$('.lines').addClass('active');
+
+	$('.main-body').addClass('active');
+});
