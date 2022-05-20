c7f0d3a9bb3901f4dddb531fc8e3b6cd39434a32
KostaSenatov <kostyasenatov@gmail.com>
2022-05-21 01:08:04 +0700

    1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 $('.points__container').ready(function(){
-	$('.main-anim').addClass('active');
 
 	$('.points__container').addClass('active');
 	$('.point').addClass('active');
 
-	$('.lines').addClass('active');
-
-	$('.main-body').addClass('active');
 });
+
+
+window.onload = function() {
+	var point = document.querySelector(".points__container");
+	var line = $(".line");
+	var mainAnim = $(".main-anim");
+	var mainBody = $(".main-body");
+	
+	TweenLite.to(point, 0.5, {
+		width: "1200px", delay: 1
+	});
+	
+	TweenLite.to(line, 0.5, {
+		top: '-20vh', delay: 4
+	});
+	
+	TweenLite.to(mainAnim, 0.5, {
+		opacity: '0', delay: 5
+	});
+	
+	TweenLite.to(mainBody, 0.5, {
+		opacity: '1', delay: 5.5
+	});
+
+}
